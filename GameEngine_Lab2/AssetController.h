80692752f22b540ba31d706afe7f024384ecc9bb
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-01-30 21:15:13 -0500

    Did some steps

--- GameEngine_Lab2/AssetController.h
+++ GameEngine_Lab2/AssetController.h
@@ -5,7 +5,20 @@
 #include "StackAllocator.h"
 #include "Asset.h"
 
-class AssetController
+class AssetController : public Singleton<AssetController>
 {
+public:
+	AssetController();
+	virtual ~AssetController();
+
+	void Initialize(int _stackSize);
+	Asset* GetAsset(string _guid);
+
+	static StackAllocator* Stack;
+
+private:
+	map<string, Asset*> m_assets;
 };
+
+#endif 
 
--- GameEngine_Lab2/GameEngine_Lab2.cpp
+++ GameEngine_Lab2/GameEngine_Lab2.cpp
@@ -1,24 +1,10 @@
-#include "Resource.h"
+#include "Asset.h"
+#include "AssetController.h"
 
 int main()
 {
-	Resource::Pool = new ObjectPool<Resource>();
-    Resource* r1 = Resource::Pool->GetResource();
-	r1->AssignNonDefaultValues();
-
-	ofstream writeStream("resource.bin", ios::out | ios::binary);
-	r1->Serialize(writeStream);
-	writeStream.close();
-	cout << "r1 values: ";
-	r1->ToString();
-	
-	Resource* r2 = Resource::Pool->GetResource();
-	ifstream readStream("resource.bin", ios::in | ios::binary);
-	r2->Deserialize(readStream);
-	readStream.close();
-	cout << "r2 values: ";
-	r1->ToString();
-	
-	delete Resource::Pool;
+	AssetController::Instance().Initialize(100000000);
+	Asset* asset = AssetController::Instance().GetAsset("emoji.jpg");
+	cout << "Bytes used by imgae.bmp: " << AssetController::Stack->GetBytesUsed() << endl;
 }
 
