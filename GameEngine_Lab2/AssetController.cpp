6dacea23fff0a87b669457b2e51f539a8b48f177
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-01-30 20:42:51 -0500

    Test

---
+++ GameEngine_Lab2/AssetController.cpp
@@ -0,0 +1,1 @@
+#include "AssetController.h"
---
+++ GameEngine_Lab2/AssetController.h
@@ -0,0 +1,11 @@
+#ifndef ASSET_CONTROLLER_H
+#define ASSET_CONTROLLER_H
+
+#include "StandardIncludes.h"
+#include "StackAllocator.h"
+#include "Asset.h"
+
+class AssetController
+{
+};
+
