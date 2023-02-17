5691de09c1df0ae481d5661a28095a29fb1fb08b
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-02-16 23:47:01 -0500

    Deleting some extra objects created

--- GameEngine_Lab2/GameController.cpp
+++ GameEngine_Lab2/GameController.cpp
@@ -31,6 +31,15 @@ void GameController::RunGame()
 	sheet->Serialize(writeStream);
 	writeStream.close();
 
+	delete SpriteAnim::Pool;
+	delete SpriteSheet::Pool;
+	AssetController::Instance().Clear();
+	AssetController::Instance().Initialize(10000000);
+	SpriteSheet::Pool = new ObjectPool<SpriteSheet>();
+	SpriteAnim::Pool = new ObjectPool<SpriteAnim>();
+
+
+
 	SpriteSheet* sheet2 = SpriteSheet::Pool->GetResource();
 	ifstream readStream("resource.bin", ios::in | ios::binary);
 	sheet2->Deserialize(readStream);
