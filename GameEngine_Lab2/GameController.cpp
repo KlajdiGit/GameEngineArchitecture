5fe606e8c94a025c6a23e50d5f67b5fd966452aa
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-02-18 18:23:10 -0500

    Output 6 complete

--- GameEngine_Lab2/GameController.cpp
+++ GameEngine_Lab2/GameController.cpp
@@ -3,6 +3,7 @@
 #include "SpriteSheet.h"
 #include "SpriteAnim.h"
 #include "TTFont.h"
+#include "Timing.h"
 
 GameController::GameController()
 {
@@ -17,6 +18,7 @@ void GameController::RunGame()
 {
 	AssetController::Instance().Initialize(10000000); //Allocate 10MB
 	Renderer* r = &Renderer::Instance();
+	Timing* t = &Timing::Instance();
 	r->Initialize(800, 600);
 	
 	TTFont* font = new TTFont();
@@ -32,43 +34,33 @@ void GameController::RunGame()
 	sheet->AddAnimation(EN_AN_IDLE, 0, 6, 0.01f);
 	sheet->AddAnimation(EN_AN_RUN, 6, 8, 0.005f);
 
-	//ofstream writeStream("resource.bin", ios::out | ios::binary);
-	//sheet->Serialize(writeStream);
-	//writeStream.close();
-
-	//delete SpriteAnim::Pool;
-	//delete SpriteSheet::Pool;
-	//AssetController::Instance().Clear();
-	//AssetController::Instance().Initialize(10000000);
-	//SpriteSheet::Pool = new ObjectPool<SpriteSheet>();
-	//SpriteAnim::Pool = new ObjectPool<SpriteAnim>();
-
-
-
-	//SpriteSheet* sheet2 = SpriteSheet::Pool->GetResource();
-	//ifstream readStream("resource.bin", ios::in | ios::binary);
-	//sheet2->Deserialize(readStream);
-	//readStream.close();
+	
 
 	while (m_sdlEvent.type != SDL_QUIT)
 	{
+		t->Tick();
+
 		SDL_PollEvent(&m_sdlEvent);
 		r->SetDrawColor(Color(255, 255, 255, 255));
 		r->ClearScreen();
 		r->RenderTexture(sheet, sheet->Update(EN_AN_IDLE), Rect(0, 0, 69*3, 44 * 3));
 		r->RenderTexture(sheet, sheet->Update(EN_AN_RUN), Rect(0, 150, 69 * 3, 150 + 44 * 3));
-		//font->Write(r->GetRenderer(), "Testing 123!!", SDL_Color{ 0,255,0 }, SDL_Point{ 150,50 });
+
 		std::string s = "Frame number: " + std::to_string(sheet->GetCurrentClip(EN_AN_IDLE));
 		font->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 50 });
 
 		s = "Frame number: " + std::to_string(sheet->GetCurrentClip(EN_AN_RUN));
 		font->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 200 });
 
+		std::string fps = "Frames Per Second: " + std::to_string(t->GetFPS());
+		font->Write(r->GetRenderer(), fps.c_str(), SDL_Color{ 0, 0, 255 }, SDL_Point{ 0, 0 });
+
 		SDL_RenderPresent(r->GetRenderer());
 	}
 
 	delete SpriteAnim::Pool;
 	delete SpriteSheet::Pool;
+	
 	font->Shutdown();
 	r->Shutdown();
 }
