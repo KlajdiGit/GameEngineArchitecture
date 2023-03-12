a8f8686bc01cdba95ecf805f9a6a046d9ebfba17
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-03-12 16:38:51 -0500

    Fixed the output 4

--- GameEngine_Lab2/GameController.cpp
+++ GameEngine_Lab2/GameController.cpp
@@ -5,6 +5,7 @@
 #include "TTFont.h"
 #include "Timing.h"
 #include "RenderTarget.h"
+#include <sstream>
 
 GameController::GameController()
 {
@@ -26,40 +27,12 @@ void GameController::RunGame()
 	TTFont* font = new TTFont();
 	font->Initialize(20);
 	
-	//Point ws = r->GetWindowSize();
-
-	//SpriteSheet::Pool = new ObjectPool<SpriteSheet>();
-	//SpriteAnim::Pool = new ObjectPool<SpriteAnim>();
-	//SpriteSheet* sheet = SpriteSheet::Pool->GetResource();
-	//sheet->Load("../Assets/Textures/Warrior.tga");
-	//sheet->SetSize(17, 6, 69, 44);
-	//sheet->AddAnimation(EN_AN_IDLE, 0, 6, 6.0f);
-	//sheet->AddAnimation(EN_AN_RUN, 6, 8, 6.0f);
-	///*sheet->SetBlendMode(SDL_BLENDMODE_BLEND);
-	//sheet->SetBlendAlpha(128);*/
-	//RenderTarget* rt = new RenderTarget();
-	//rt->Create(ws.X, ws.Y);
-
+	
 	while (m_sdlEvent.type != SDL_QUIT)
 	{
 		t->Tick();
 		r->SetDrawColor(Color(255, 255, 255, 255));
 		r->ClearScreen();
-
-
-		//rt->Start();
-
-		//SDL_PollEvent(&m_sdlEvent);
-	/*	r->SetDrawColor(Color(255, 255, 255, 255));
-		r->ClearScreen();
-		r->RenderTexture(sheet, sheet->Update(EN_AN_IDLE, t->GetDeltaTime()), Rect(0, 0, 69*3, 44 * 3));
-		r->RenderTexture(sheet, sheet->Update(EN_AN_RUN, t->GetDeltaTime()), Rect(0, 150, 69 * 3, 150 + 44 * 3));*/
-
-		/*std::string s = "Frame number: " + std::to_string(sheet->GetCurrentClip(EN_AN_IDLE));
-		font->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 50 });
-
-		s = "Frame number: " + std::to_string(sheet->GetCurrentClip(EN_AN_RUN));
-		font->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 200 });*/
 
 		std::string fps = "Frames Per Second: " + std::to_string(t->GetFPS());
 		font->Write(r->GetRenderer(), fps.c_str(), SDL_Color{ 0, 0, 255 }, SDL_Point{ 0, 0 });
@@ -69,24 +42,15 @@ void GameController::RunGame()
 		{
 			stringstream s;
 			s << modes[count].w << "x" << modes[count].h << " " << modes[count].refresh_rate << "hz";
-			font->Write(r->GetRenderer(), s.str(), SDL_Color{ 255, 128, 0 }, SDL_Point{ 0, 2 - +count * 20 });
+			font->Write(r->GetRenderer(), s.str().c_str(), SDL_Color{ 255, 128, 0 }, SDL_Point{ 0, 20 +count * 20 });
 
 		}
-
-
-		/*rt->Stop();
-		r->SetDrawColor(Color(0, 0, 0, 255));
-		r->ClearScreen();
-		rt->Render(t->GetDeltaTime());*/
 
 		SDL_RenderPresent(r->GetRenderer());
 		t->CapFPS();
 	}
 
-	//delete rt;
-	//delete SpriteAnim::Pool;
-	//delete SpriteSheet::Pool;
-	//
+
 	font->Shutdown();
 	r->Shutdown();
 }
