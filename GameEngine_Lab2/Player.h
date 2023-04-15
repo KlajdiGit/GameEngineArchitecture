1248bfa75ea347c0058c7753b6a28d71a0327893
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-04-15 15:57:13 -0400

    test

---
+++ GameEngine_Lab2/Player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "PlayerState.h"
+#include "RigidBody.h"
+
+class SpriteSheet;
+class Renderer;
+
+
+class Player
+{
+public:
+	Player();
+	virtual ~Player();
+
+	PlayerState* GetState() { return m_state; }
+	void SetState(PlayerState* _state) { m_state = _state; }
+	SpriteSheet* GetAnimations() { return m_animations; }
+	Rect* GetSrcRect() { return &m_srcRect; }
+
+	virtual void HandleInput(SDL_Event _event, float _deltaTime);
+	virtual void Update(float _deltaTime);
+	virtual void Render(Renderer* _renderer);
+
+private:
+	RigidBody* m_rigidBody;
+	PlayerState* m_state;
+	SpriteSheet* m_animations;
+	Rect m_srcRect;
+
+};
+
+#endif //PLAYER_H
---
+++ GameEngine_Lab2/PlayerState.h
@@ -0,0 +1,94 @@
+#ifndef PLAYER_STATE_H
+#define PLAYER_STATE_H
+
+#include "StandardIncludes.h"
+
+
+/*State 1: Idle – clip 0
+• State 2: Roll – clips 0 to 3
+• State 3: Paper – clips 4 to 7
+• State 4: Scissors – clips 8 – 11
+• State 5: Rock – clips 12 - 15*/
+
+
+class Player;
+class IdleState;
+class RollState;
+class RockState;
+class PaperState;
+class ScissorState;
+
+class PlayerState
+{
+public:
+	PlayerState() { }
+	virtual ~PlayerState() { }
+
+	static IdleState* GetIdleState() { return &m_idleState; }
+	static RollState* GetRollState() { return &m_rollState; }
+	static RockState* GetRockState() { return &m_rockState; }
+	static PaperState* GetPaperState() { return &m_paperState; }
+	static ScissorState* GetScissorState() { return &m_scissorState; }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) = 0;
+	virtual void Update(Player* _player, float _deltaTIme) = 0;
+
+private:
+	static IdleState m_idleState;
+	static RollState m_rollState;
+	static RockState m_rockState;
+	static PaperState m_paperState;
+	static ScissorState m_scissorState;
+};
+
+class IdleState : public PlayerState
+{
+public:
+	IdleState() { }
+	virtual ~IdleState() { }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) override;
+	virtual void Update(Player* _player, float _deltaTIme) override;
+};
+
+class RollState : public PlayerState
+{
+public:
+	RollState() { }
+	virtual ~RollState() { }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) override;
+	virtual void Update(Player* _player, float _deltaTIme) override;
+};
+
+class RockState : public PlayerState
+{
+public:
+	RockState() { }
+	virtual ~RockState() { }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) override;
+	virtual void Update(Player* _player, float _deltaTIme) override;
+};
+
+class PaperState : public PlayerState
+{
+public:
+	PaperState() { }
+	virtual ~PaperState() { }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) override;
+	virtual void Update(Player* _player, float _deltaTIme) override;
+};
+
+class ScissorState : public PlayerState
+{
+public:
+	ScissorState() { }
+	virtual ~ScissorState() { }
+
+	virtual void HandleInput(Player* _player, SDL_Event _event) override;
+	virtual void Update(Player* _player, float _deltaTIme) override;
+};
+
+#endif //PLAYER_STATE_H
