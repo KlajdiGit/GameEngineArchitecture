de7523b65fe339c44ce1b469421c3ea27fd8af9c
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-04-15 15:55:28 -0400

    test commit

---
+++ GameEngine_Lab2/Player.cpp
@@ -0,0 +1,39 @@
+#include "Player.h"
+#include "SpriteSheet.h"
+#include "Renderer.h"
+#include "PhysicsController.h"
+
+Player::Player()
+{
+	m_state = PlayerState::GetIdleState();
+	m_animations = SpriteSheet::Pool->GetResource();
+	m_animations->Load("../Assets/Textures/RockPaperScissors.tga");
+	m_animations->SetSize(4, 4, 280, 170);
+	m_animations->AddAnimation(EN_AN_IDLE, 0, 0, 4.0f);
+	m_animations->AddAnimation(EN_AN_ROLL, 0, 3, 4.0f);
+	m_animations->AddAnimation(EN_AN_ROCK, 4, 4, 4.0f);
+	m_animations->AddAnimation(EN_AN_PAPER, 8, 4, 4.0f);
+	m_animations->AddAnimation(EN_AN_SCISSOR, 12, 4, 4.0f);
+	m_animations->SetBlendMode(SDL_BLENDMODE_BLEND);
+}
+
+Player::~Player()
+{
+}
+
+void Player::HandleInput(SDL_Event _event, float _deltaTime)
+{
+	m_state->HandleInput(this, _event);
+}
+
+void Player::Update(float _deltaTime)
+{
+	m_state->Update(this, _deltaTime);
+}
+
+void Player::Render(Renderer* _renderer)
+{
+	glm::vec2 p = { 200, 200 };
+	Rect destRect = Rect(p.x, p.y, p.x + 280, p.y + 170);
+	_renderer->RenderTexture(m_animations, m_srcRect, destRect);
+}
---
+++ GameEngine_Lab2/PlayerState.cpp
@@ -0,0 +1,89 @@
+#include "PlayerState.h"
+#include "InputController.h"
+#include "Keyboard.h"
+#include "Player.h"
+#include "SpriteSheet.h"
+
+
+/*State 1: Idle – clip 0
+• State 2: Roll – clips 0 to 3
+• State 3: Paper – clips 4 to 7
+• State 4: Scissors – clips 8 – 11
+• State 5: Rock – clips 12 - 15*/
+
+IdleState PlayerState::m_idleState;
+RollState RollState::m_rollState;
+RockState PlayerState::m_rockState;
+PaperState PlayerState::m_paperState;
+ScissorState PlayerState::m_scissorState;
+
+void IdleState::HandleInput(Player* _player, SDL_Event _event)
+{
+	if (InputController::Instance().KB()->KeyUp(_event, SDLK_SPACE))
+	{
+		_player->GetAnimations()->ResetCurrentClip(EN_AN_IDLE);
+		_player->SetState(PlayerState::GetIdleState());
+	}
+}
+
+void IdleState::Update(Player* _player, float _deltaTime)
+{
+	_player->GetAnimations()->Update(EN_AN_IDLE, _deltaTime, _player->GetSrcRect());
+}
+
+void RollState::HandleInput(Player* _player, SDL_Event _event)
+{
+	if (InputController::Instance().KB()->KeyUp(_event, SDLK_SPACE))
+	{
+		_player->GetAnimations()->ResetCurrentClip(EN_AN_ROLL);
+		_player->SetState(PlayerState::GetRollState());
+	}
+}
+
+void RollState::Update(Player* _player, float _deltaTime)
+{
+	_player->GetAnimations()->Update(EN_AN_ROLL, _deltaTime, _player->GetSrcRect());
+}
+
+
+void RockState::HandleInput(Player* _player, SDL_Event _event)
+{
+	if (InputController::Instance().KB()->KeyUp(_event, SDLK_SPACE))
+	{
+		_player->GetAnimations()->ResetCurrentClip(EN_AN_ROCK);
+		_player->SetState(PlayerState::GetRockState());
+	}
+}
+
+void RockState::Update(Player* _player, float _deltaTime)
+{
+	_player->GetAnimations()->Update(EN_AN_ROCK, _deltaTime, _player->GetSrcRect());
+}
+
+void PaperState::HandleInput(Player* _player, SDL_Event _event)
+{
+	if (InputController::Instance().KB()->KeyUp(_event, SDLK_SPACE))
+	{
+		_player->GetAnimations()->ResetCurrentClip(EN_AN_PAPER);
+		_player->SetState(PlayerState::GetRollState());
+	}
+}
+
+void PaperState::Update(Player* _player, float _deltaTime)
+{
+	_player->GetAnimations()->Update(EN_AN_PAPER, _deltaTime, _player->GetSrcRect());
+}
+
+void ScissorState::HandleInput(Player* _player, SDL_Event _event)
+{
+	if (InputController::Instance().KB()->KeyUp(_event, SDLK_SPACE))
+	{
+		_player->GetAnimations()->ResetCurrentClip(EN_AN_SCISSOR);
+		_player->SetState(PlayerState::GetRollState());
+	}
+}
+
+void ScissorState::Update(Player* _player, float _deltaTime)
+{
+	_player->GetAnimations()->Update(EN_AN_SCISSOR, _deltaTime, _player->GetSrcRect());
+}
